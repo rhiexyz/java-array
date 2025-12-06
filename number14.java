2fe265dab1aa70b207f84785c190f6ef4a518105
Ruth <gonzalesruth0824@gmail.com>
2025-12-06 11:26:10 -0500

    Update number14.java

--- number14.java
+++ number14.java
@@ -11,7 +11,7 @@ public class number14
         int[] arr = {1, 2, 3, 4, 5};
 
     for (int x = arr.length - 1; x >= 0; x--) {
-    System.out.println(arr[x]);
+    System.out.print(arr[x] + " ");
     }
   }
 }
