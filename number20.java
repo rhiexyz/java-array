1a2516f10d97716e35cab9518e0257fd577780e4
Ruth <gonzalesruth0824@gmail.com>
2025-12-06 11:34:13 -0500

    Update number20.java

--- number20.java
+++ number20.java
@@ -1,4 +1,3 @@
-
 /**
  * Write a description of class number20 here.
  *
@@ -7,13 +6,15 @@
  */
 public class number20
 {
-   public static void main(String[] args) {
-       int[] arr = {10, 22, 35, 40, 18, 50};
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 35, 40, 18, 50};
 
-    for (int x : arr) {
-    if (x > 20 && x % 2 == 0) {
-        System.out.print(x + " ");
-     }
-   }
- }
+        System.out.print("Even numbers greater than 20: ");
+
+        for (int x : arr) {
+            if (x > 20 && x % 2 == 0) {
+                System.out.print(x + " ");
+            }
+        }
+    }
 }
