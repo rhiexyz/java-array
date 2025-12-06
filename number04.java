4d9d3d1e9037efb9c9a857d745b9b237c9de932a
Ruth <gonzalesruth0824@gmail.com>
2025-12-06 11:15:06 -0500

    Update number04.java

--- number04.java
+++ number04.java
@@ -5,18 +5,29 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import javax.swing.*;
 public class number4
 {
-    public static void main (String [] args){
-        int[] arr= {5,9,1,3,8};
-        int min = arr[0];
-        int max = arr[0];
-        
-        for (int x: arr){
-            if(x < min)min = x;
-            if(x > max)max = x;
+    public static void main(String[] args) {
+        int[] num = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            num[i] = Integer.parseInt(JOptionPane.showInputDialog("Enter number #" + (i+1) + ":"));
         }
-            System.out.println("Min: " + min);
-            System.out.println("Max: " + max);
+
+        int smallest = num[0];
+        int largest = num[0];
+
+        for (int i = 1; i < 3; i++) {
+            if (num[i] < smallest) {
+                smallest = num[i];
+            }
+            if (num[i] > largest) {
+                largest = num[i];
+            }
         }
+
+        JOptionPane.showMessageDialog(null, "Smallest: " + smallest + "\nLargest: " + largest);
     }
+}
+
